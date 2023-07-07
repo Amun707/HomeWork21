4e4dcce1241d622e7429c90128ce3f718255daf9
Amun707 <120380281+Amun707@users.noreply.github.com>
2023-07-07 15:02:43 +0500

    Добавьте файлы проекта.

---
+++ HomeWork21/Main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+int main() {
+	system("chcp 1251>nul");
+	int n, m;
+	std::cout << "Введите два числа: ";
+	std::cin >> n, m;
+	int* pn = &n;
+	int* pm = &m;
+	int result = (*pn + *pm) / 2;
+	std::cout << "Среднее арифметическое = " << result << std::endl;
+
+
+
+
+	return 0;
+}
